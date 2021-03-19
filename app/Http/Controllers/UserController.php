676c7bafc77d5a373380fd15afc89fa7fd6826c6
JoseAlejandroGarciaCifuentes <jose_alejandro_garcia_apps1ma1920@cev.com>
2021-03-19 20:02:46 +0100

    mission spin to win not showing anymore

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -686,7 +686,7 @@ class UserController extends ApiController
             //generateRandom
             $new_mission_id = rand(1, count($missions));
             
-        }while(in_array($new_mission_id , $user_missions_id) || $new_mission_id==$request->get('id') || $new_mission_id == 11 || $new_mission_id == 13);
+        }while(in_array($new_mission_id , $user_missions_id) || $new_mission_id==$request->get('id') || $new_mission_id == 12 || $new_mission_id == 13);
         
 
         $new_user_mission = userMission::create([
